cda41cf05e18dd75b36300be55588b937591707a
Julia Maximov <julia.maximov18@gmail.com>
2026-09-05 23:43:09 -0400

    added javascript and come functionality 9/5/26

---
+++ index.js
@@ -0,0 +1,33 @@
+ 
+ 
+ function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading')
+  const success = document.querySelector('.modal__overlay--success')
+  loading.classList += " modal__overlay--visible"
+  emailjs
+    .sendForm(
+      'service_djk5vlu', 
+      'template_yoz6ouk', 
+     event.target, 
+      'B-hZmuYtGqZrNdam2'
+    ).then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+      loading.classList.remove("modal__0verlay--visible");
+      alert(
+        "The email service is temperarily unavailable. Please contact me directly at julia.maximov18@gmail.com"
+      );
+    })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen){
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+   isModalOpen = true;
+  document.body.classList += " modal--open";
+}
